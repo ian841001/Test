445b18090a8e10ddfecbdf65dac78b619e0735d9
ian841001 <ian841001@gmail.com>
2017-10-05 00:34:08 +0800

    1.2

--- src/MyJPanel.java
+++ src/MyJPanel.java
@@ -1,30 +1,34 @@
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class MyJPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 	BufferedImage bufferedImage;
 
-	public MyJPanel(Mat frame) {
-		int type = 0;
-        if (frame.channels() == 1) {
-            type = BufferedImage.TYPE_BYTE_GRAY;
-        } else if (frame.channels() == 3) {
-            type = BufferedImage.TYPE_3BYTE_BGR;
-        } else {
-        	throw new RuntimeException(String.valueOf(frame.channels()));
-        }
-        bufferedImage = new BufferedImage(frame.width(), frame.height(), type);
-        WritableRaster raster = bufferedImage.getRaster();
-        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
-        byte[] data = dataBuffer.getData();
-        frame.get(0, 0, data);
+	public MyJPanel(Mat frame) throws IOException {
+//		int type = 0;
+//        if (frame.channels() == 1) {
+//            type = BufferedImage.TYPE_BYTE_GRAY;
+//        } else if (frame.channels() == 3) {
+//            type = BufferedImage.TYPE_3BYTE_BGR;
+//        } else {
+//        	throw new RuntimeException(String.valueOf(frame.channels()));
+//        }
+//        bufferedImage = new BufferedImage(frame.width(), frame.height(), type);
+//        WritableRaster raster = bufferedImage.getRaster();
+//        DataBufferByte dataBuffer = (DataBufferByte) raster.getDataBuffer();
+//        byte[] data = dataBuffer.getData();
+//        frame.get(0, 0, data);
+		bufferedImage = mat2BufferedImage(frame);
         
 	}
 	public MyJPanel(BufferedImage frame) {
@@ -35,4 +39,14 @@ public class MyJPanel extends JPanel {
     public void paint(Graphics g) {
         g.drawImage(bufferedImage, 0, 0, this);
     }
+
+	static BufferedImage mat2BufferedImage(Mat matrix) throws IOException {        
+	    MatOfByte mob=new MatOfByte();
+	    Imgcodecs.imencode(".jpg", matrix, mob);
+	    byte ba[]=mob.toArray();
+
+	    BufferedImage bi=ImageIO.read(new ByteArrayInputStream(ba));
+	    return bi;
+	}
+	
 }
